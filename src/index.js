9d500a978a80ada926338ed810754528508b43a3
Artem Valkov <valkov.artyom@gmail.com>
2020-02-19 15:20:14 +0200

    footer added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './assets/css/main.css';
 import './assets/scss/main.scss';
 import './assets/scss/header.scss';
+import './assets/scss/footer.scss';
 import './assets/scss/fonts.scss';
 import './assets/scss/animation.scss'
 import './js/script';
